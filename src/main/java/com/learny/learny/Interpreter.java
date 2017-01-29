3a09d726c929eb969b12b238f5d0a23c5b281d67
Skrinikov <skrinikovd@gmail.com>
2017-01-29 06:17:27 -0500

    Fix issues

--- src/main/java/com/learny/learny/Interpreter.java
+++ src/main/java/com/learny/learny/Interpreter.java
@@ -51,43 +51,43 @@ public class Interpreter {
         //String testString="The bank continued to mislead shareholders in its annual reports of January 2008 and April 2009, both of which identified Sheikh Mansour as the investor.";
         //String testString = "Unlike from RBS and Lloyds TSB, Barclays narrowly avoided having to request a government bailout late in 2008 after it was rescued by $7bn worth of new investment, most of which came from the gulf states of Qatar and Abu Dhabi.";
         //String testString="Martin Luther King Jr was a great black man.";
-        String testString="Barclays misled shareholders and the public about one of the biggest investments in the bank's history, a BBC Panorama investigation has found.\n" +
-"The bank announced in 2008 that Manchester City owner Sheikh Mansour had agreed to invest more than £3bn.\n" +
-"But the BBC found that the money, which helped Barclays avoid a bailout by British taxpayers, actually came from the Abu Dhabi government.\n" +
-"Barclays said the mistake in its accounts was \"a drafting error\".\n" +
-"Unlike RBS and Lloyds TSB, Barclays narrowly avoided having to request a government bailout late in 2008 after it was rescued by £7bn worth of new investment, most of which came from the gulf states of Qatar and Abu Dhabi.\n" +
-"Half of the cash was supposed to be coming from Sheikh Mansour.\n" +
-"But Barclays has admitted it was told the investor might change shortly before shareholders voted to approve the deal on 24 November 2008.\n" +
-"But instead of telling shareholders, the bank remained silent until the change of investor was confirmed a few hours later.\n" +
-"\n" +
-"The bank said it subsequently provided \"appropriate disclosure\" in three prospectuses that were issued the following day.\n" +
-"But the disclosure was buried deep in the small print and said that Sheikh Mansour \"has arranged for his investment…to be funded by an Abu Dhabi governmental investment vehicle, which will become the indirect shareholder\".\n" +
-"Barclays still used the phrase \"his investment\", even though it knew Sheikh Mansour was not actually investing in the bank at the time.\n" +
-"The bank continued to mislead shareholders in its annual reports of 2008 and 2009, both of which identified Sheikh Mansour as the investor\n" +
-"Barclays said the mistake in its accounts was \"simply a drafting error\" and that the information provided in the prospectuses was \"entirely appropriate in all the circumstances\".\n" +
-"The bank also said: \"The shareholders meeting had already taken place and there was therefore no need to issue press releases or additional formal communications to shareholders/other market participants.\"\n" +
-"\n" +
-"Professor Alistair Milne, an expert on financial regulation in the City, said banks are expected to release accurate information about major deals.\n" +
-"\"Any discrepancy of that kind is serious because it raises questions in the minds of investors. Every bank is well aware the annual report is a critical document and a huge amount of time and attention is put in to trying to get all the details correct.\"\n" +
-"The Abu Dhabi government investment vehicle that provided the funding, International Petroleum Investment Company (IPIC), was not mentioned in any Barclays' announcements until six months after the deal.\n" +
-"\n" +
-"The chairman of IPIC is Sheikh Mansour. Although the Sheikh did not invest any of his own money in Barclays at the time, a company he controlled was initially issued warrants to buy 758 million shares in the bank.\n" +
-"The warrants gave the owner a valuable option to buy the shares at a fixed price at any point over the following five years.\n" +
-"If Sheikh Mansour profited personally, then Barclays may have breached anti-corruption laws aimed at preventing payments to government officials.\n" +
-"Jeremy Carver, a lawyer for Transparency International, said Barclays may be at fault if government officials benefitted from the deal.\n" +
-"\"You have to worry not because Sheikh Mansour may or may not be doing something wrong, you have to worry because you may be doing something wrong as a bank.\n" +
-"\"You may be committing a crime, you may be paying a bribe if you have not got it straight as to which capacity the person you are dealing with is acting.\"\n" +
-"\n" +
-"Barclays issued the warrants to a Jersey company called PCP Gulf Invest 3, which represented the beneficial interests of Sheikh Mansour.\n" +
-"Control of the company was then transferred from the Sheikh to IPIC, then from IPIC to an Abu Dhabi official and then eventually back to Sheikh Mansour.\n" +
-"It is impossible to tell who benefited from the warrants, because all of these transactions took place offshore.\n" +
-"However, the 758 million shares associated with the warrants were bought at significantly below the market price and they now belong to Sheikh Mansour.\n" +
-"Barclays said the change in ownership of the offshore company had no bearing on the transaction or required approvals.\n" +
-"The bank said in a statement that it had \"repeatedly demonstrated to Panorama why the allegations which have been put to us are not justified\".\n" +
-"\"The Board of Barclays took the decision on capital raising in 2008 on the basis of the best interest of shareholder and its other stakeholders, including UK taxpayers,\" it said.\n" +
-"\"Barclays performance relative to other UK banks which accepted government funding, especially on key measures such as lending growth, demonstrates unequivocally that it was the correct decision for Barclays, its shareholders, its customers and clients, as well as the UK.\"\n" +
-"Neither Sheikh Mansour nor IPIC responded to questions raised by Panorama.\n" +
-"In August last year, the UK's Serious Fraud Office said it had started an investigation into commercial arrangements between the bank and Qatar Holding LLC, part of sovereign wealth fund Qatar Investment Authority.";
+        String testString = "Barclays misled shareholders and the public about one of the biggest investments in the bank's history, a BBC Panorama investigation has found.\n"
+                + "The bank announced in 2008 that Manchester City owner Sheikh Mansour had agreed to invest more than £3bn.\n"
+                + "But the BBC found that the money, which helped Barclays avoid a bailout by British taxpayers, actually came from the Abu Dhabi government.\n"
+                + "Barclays said the mistake in its accounts was \"a drafting error\".\n"
+                + "Unlike RBS and Lloyds TSB, Barclays narrowly avoided having to request a government bailout late in 2008 after it was rescued by £7bn worth of new investment, most of which came from the gulf states of Qatar and Abu Dhabi.\n"
+                + "Half of the cash was supposed to be coming from Sheikh Mansour.\n"
+                + "But Barclays has admitted it was told the investor might change shortly before shareholders voted to approve the deal on 24 November 2008.\n"
+                + "But instead of telling shareholders, the bank remained silent until the change of investor was confirmed a few hours later.\n"
+                + "\n"
+                + "The bank said it subsequently provided \"appropriate disclosure\" in three prospectuses that were issued the following day.\n"
+                + "But the disclosure was buried deep in the small print and said that Sheikh Mansour \"has arranged for his investment…to be funded by an Abu Dhabi governmental investment vehicle, which will become the indirect shareholder\".\n"
+                + "Barclays still used the phrase \"his investment\", even though it knew Sheikh Mansour was not actually investing in the bank at the time.\n"
+                + "The bank continued to mislead shareholders in its annual reports of 2008 and 2009, both of which identified Sheikh Mansour as the investor\n"
+                + "Barclays said the mistake in its accounts was \"simply a drafting error\" and that the information provided in the prospectuses was \"entirely appropriate in all the circumstances\".\n"
+                + "The bank also said: \"The shareholders meeting had already taken place and there was therefore no need to issue press releases or additional formal communications to shareholders/other market participants.\"\n"
+                + "\n"
+                + "Professor Alistair Milne, an expert on financial regulation in the City, said banks are expected to release accurate information about major deals.\n"
+                + "\"Any discrepancy of that kind is serious because it raises questions in the minds of investors. Every bank is well aware the annual report is a critical document and a huge amount of time and attention is put in to trying to get all the details correct.\"\n"
+                + "The Abu Dhabi government investment vehicle that provided the funding, International Petroleum Investment Company (IPIC), was not mentioned in any Barclays' announcements until six months after the deal.\n"
+                + "\n"
+                + "The chairman of IPIC is Sheikh Mansour. Although the Sheikh did not invest any of his own money in Barclays at the time, a company he controlled was initially issued warrants to buy 758 million shares in the bank.\n"
+                + "The warrants gave the owner a valuable option to buy the shares at a fixed price at any point over the following five years.\n"
+                + "If Sheikh Mansour profited personally, then Barclays may have breached anti-corruption laws aimed at preventing payments to government officials.\n"
+                + "Jeremy Carver, a lawyer for Transparency International, said Barclays may be at fault if government officials benefitted from the deal.\n"
+                + "\"You have to worry not because Sheikh Mansour may or may not be doing something wrong, you have to worry because you may be doing something wrong as a bank.\n"
+                + "\"You may be committing a crime, you may be paying a bribe if you have not got it straight as to which capacity the person you are dealing with is acting.\"\n"
+                + "\n"
+                + "Barclays issued the warrants to a Jersey company called PCP Gulf Invest 3, which represented the beneficial interests of Sheikh Mansour.\n"
+                + "Control of the company was then transferred from the Sheikh to IPIC, then from IPIC to an Abu Dhabi official and then eventually back to Sheikh Mansour.\n"
+                + "It is impossible to tell who benefited from the warrants, because all of these transactions took place offshore.\n"
+                + "However, the 758 million shares associated with the warrants were bought at significantly below the market price and they now belong to Sheikh Mansour.\n"
+                + "Barclays said the change in ownership of the offshore company had no bearing on the transaction or required approvals.\n"
+                + "The bank said in a statement that it had \"repeatedly demonstrated to Panorama why the allegations which have been put to us are not justified\".\n"
+                + "\"The Board of Barclays took the decision on capital raising in 2008 on the basis of the best interest of shareholder and its other stakeholders, including UK taxpayers,\" it said.\n"
+                + "\"Barclays performance relative to other UK banks which accepted government funding, especially on key measures such as lending growth, demonstrates unequivocally that it was the correct decision for Barclays, its shareholders, its customers and clients, as well as the UK.\"\n"
+                + "Neither Sheikh Mansour nor IPIC responded to questions raised by Panorama.\n"
+                + "In August last year, the UK's Serious Fraud Office said it had started an investigation into commercial arrangements between the bank and Qatar Holding LLC, part of sovereign wealth fund Qatar Investment Authority.";
         //System.out.println(testString);
         try {
             AnalyzedText at = tr.analyze(testString);
@@ -107,24 +107,24 @@ public class Interpreter {
                         }
                     }
                 }
-                List<String> tempList=new ArrayList<>();
-                for(int i=0;i<properNames.size();i++){
-                    if(!tempList.contains(properNames.get(i))){
+                List<String> tempList = new ArrayList<>();
+                for (int i = 0; i < properNames.size(); i++) {
+                    if (!tempList.contains(properNames.get(i))) {
                         tempList.add(properNames.get(i));
                     }
                 }
-                properNames=tempList;
+                properNames = tempList;
                 /*
                 Set<String> nounsSet=new HashSet<>();
                 nounsSet.addAll(properNames);
                 properNames.clear();
                 properNames.addAll(nounsSet);
-                */
+                 */
             }
-            for(int i=properNames.size()-1;i>-1;i--){
-                    System.out.println("NOUNS: "+properNames.get(i));
-                }
-            
+            for (int i = properNames.size() - 1; i > -1; i--) {
+                System.out.println("NOUNS: " + properNames.get(i));
+            }
+
         } catch (Exception e) {
             System.out.println("ERROR -" + e.getMessage() + e.getClass());
         }
@@ -313,60 +313,62 @@ public class Interpreter {
         }
         return false;
     }
-    
+
     /**
      * Retrives the relevant keywords of the document.
+     *
      * @param text String
      * @return List of keywords
      */
     public List<String> getKeyWords(String text) {
         List<String> keywords = new ArrayList<>();
-        
+
         try {
             AnalyzedText at = tr.analyze(text);
             keywords = getMostRelevantEntities(at.getResponse());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
+
         return keywords;
     }
-    
+
     /**
-     * Extract relevant keywords from the input text 
+     * Extract relevant keywords from the input text
+     *
      * @param rp Response
      * @return List of keywords
      */
-    private List<String> getMostRelevantEntities(Response rp){
-        List<Entity> entities =  rp.getEntities();
+    private List<String> getMostRelevantEntities(Response rp) {
+        List<Entity> entities = rp.getEntities();
         List<String> subjects = new ArrayList<>();
-        if (entities != null)
-        {
-            for(Entity entity : entities){
-                if (entity.getRelevanceScore() > 0.4 && entity.getConfidenceScore() > 2)
-                {
-                    if (!listContains(subjects, entity.getMatchedText())){
-                        System.out.println("Entity: " + entity.getMatchedText() + " - Score: "+ entity.getRelevanceScore() );
+        if (entities != null) {
+            for (Entity entity : entities) {
+                if (entity.getRelevanceScore() > 0.4 && entity.getConfidenceScore() > 2) {
+                    if (!listContains(subjects, entity.getMatchedText())) {
+                        System.out.println("Entity: " + entity.getMatchedText() + " - Score: " + entity.getRelevanceScore());
                         subjects.add(entity.getMatchedText());
                     }
                 }
             }
         }
-        
+
         return subjects;
-        
+
     }
-    
+
     /**
      * Check if list contains a certain value
+     *
      * @param set
      * @param value
-     * @return 
+     * @return
      */
-    private boolean listContains (List<String> set, String value){
-        for (String data : set){
-            if (data.equalsIgnoreCase(value))
+    private boolean listContains(List<String> set, String value) {
+        for (String data : set) {
+            if (data.equalsIgnoreCase(value)) {
                 return true;
+            }
         }
         return false;
     }
@@ -414,26 +416,7 @@ public class Interpreter {
         }
         return properNoun;
     }*/
-
  /*
-    private String retrieveNounPhrase(Word word){
-        for(NounPhrase np:word.getNounPhrases()){
-            
-        }
-    }*/
-    private boolean isDate(Entity entity) {
-        System.out.println("IN ISDATE");
-        for (String type : entity.getDBPediaTypes()) {
-            System.out.println("TYPE: " + type);
-            if (type.equalsIgnoreCase("time")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    /*
     private String retrieveNounPhrase(Word word){
         for(NounPhrase np:word.getNounPhrases()){
             
@@ -555,9 +538,10 @@ public class Interpreter {
             System.out.println("Time");
             return true;
         }
-        if(pos >= 1 && words.get(pos - 1).getPartOfSpeech().equals("IN") && pos + 1 < words.size() && words.get(pos+1).getToken().matches("[,.]")){
-            if(words.get(pos).getEntities().get(0).getDBPediaTypes().get(0).equalsIgnoreCase("Time"))
+        if (pos >= 1 && words.get(pos - 1).getPartOfSpeech().equals("IN") && pos + 1 < words.size() && words.get(pos + 1).getToken().matches("[,.]")) {
+            if (words.get(pos).getEntities().get(0).getDBPediaTypes().get(0).equalsIgnoreCase("Time")) {
                 return true;
+            }
         }
         if (pos + 1 < words.size() && words.get(pos).getPartOfSpeech().equals("CD")) {
             if (words.get(pos + 1).getPartOfSpeech().matches("VB[DGNPZ]") || words.get(pos + 1).getPartOfSpeech().equals("TO") || words.get(pos + 1).getPartOfSpeech().equals("IN") || words.get(pos + 1).getPartOfSpeech().equals("CC")) {
