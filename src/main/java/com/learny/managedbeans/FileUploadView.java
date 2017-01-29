3059590a1a5632d9cab0b48faa69892f518e9270
Skrinikov <skrinikovd@gmail.com>
2017-01-29 00:41:43 -0500

    Styling

--- src/main/java/com/learny/managedbeans/FileUploadView.java
+++ src/main/java/com/learny/managedbeans/FileUploadView.java
@@ -1,5 +1,7 @@
 package com.learny.managedbeans;
  
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -10,10 +12,24 @@ import org.primefaces.model.UploadedFile;
 @ManagedBean
 public class FileUploadView {
  
+    private List<String> data = new ArrayList<>();
+    
     public void handleFileUpload(FileUploadEvent event) {
         String fileContents = new String(event.getFile().getContents());
         
-        FacesMessage message = new FacesMessage("Succesful", fileContents + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, message);
+        /*FacesMessage message = new FacesMessage("Succesful", fileContents + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);*/
+        
+        data.add(fileContents);
     }
+
+    public List<String> getData() {
+        return data;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+    
+    
 }
