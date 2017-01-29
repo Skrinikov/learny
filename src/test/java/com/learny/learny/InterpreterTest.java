2d5b8278d055114dc63dbab7339da86c21783e96
Hau Gilles Che <haugillesche@gmail.com>
2017-01-29 10:03:24 -0500

    added string to analyze in testing unit

--- src/test/java/com/learny/learny/InterpreterTest.java
+++ src/test/java/com/learny/learny/InterpreterTest.java
@@ -41,12 +41,16 @@ public class InterpreterTest {
     /**
      * Test of analyzeTest method, of class Interpreter.
      */
-    @Ignore
     @Test
     public void testAnalyzeTest() throws Exception{
         System.out.println("analyzeTest");
         Interpreter instance = new Interpreter();
-        instance.analyze("Hello JAcob, how are you.");
+        instance.analyze("Barclays said the change in ownership of the offshore company had no bearing on the transaction or required approvals.\n" +
+"The bank said in a statement that it had \"repeatedly demonstrated to Panorama why the allegations which have been put to us are not justified\".\n" +
+"\"The Board of Barclays took the decision on capital raising in 2008 on the basis of the best interest of shareholder and its other stakeholders, including UK taxpayers,\" it said.\n" +
+"\"Barclays performance relative to other UK banks which accepted government funding, especially on key measures such as lending growth, demonstrates unequivocally that it was the correct decision for Barclays, its shareholders, its customers and clients, as well as the UK.\"\n" +
+"Neither Sheikh Mansour nor IPIC responded to questions raised by Panorama.\n" +
+"In August last year, the UK's Serious Fraud Office said it had started an investigation into commercial arrangements between the bank and Qatar Holding LLC, part of sovereign wealth fund Qatar Investment Authority.");
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
